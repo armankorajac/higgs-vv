72aa15393eb997d5ecefa6902cfcab799d9f9f68
armankorajac <armankorajac@gmail.com>
2025-09-24 14:48:59 +0200

    Pisa basis - UFO upload

---
+++ higgsVV_pisa_basis_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.3.1 for Microsoft Windows (64-bit) (June 24, 2021)
+# Date: Wed 24 Sep 2025 14:47:01
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ higgsVV_pisa_basis_UFO/coupling_orders.py
@@ -0,0 +1,32 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.3.1 for Microsoft Windows (64-bit) (June 24, 2021)
+# Date: Wed 24 Sep 2025 14:47:01
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+cHB = CouplingOrder(name = 'cHB',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+cHW = CouplingOrder(name = 'cHW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+cHWB = CouplingOrder(name = 'cHWB',
+                     expansion_order = 99,
+                     hierarchy = 1)
+
